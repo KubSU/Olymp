fc562913db29a4b1f60bcc6c9e1f4b0f613105a1
EugeneButusov <EvgenyButusov@gmail.com>
2013-10-20 15:01:26 +0400

    Версия оригинального Graph.cpp от 19.10.2013

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -49,12 +49,78 @@ struct comparer
     }
 };
 
+typedef pair <int, WeightType> dijkstra_elem;
+
+struct dijkstra_comparer
+{
+	bool operator()(const dijkstra_elem &x, const dijkstra_elem &y) const
+	{
+		return x.second < y.second;
+	}
+};
 
 typedef vector< set<Connected, comparer> > Graph;
 
 vector<Vertex> vertexInfo;
 vector < vector<int> > ResultVertexes;
 Graph graph;
+
+
+int AddEdge(int v1, int v2, WeightType weight)
+{
+    graph[v1].insert(Connected(v2, weight, 0));
+    graph[v2].insert(Connected(v1, weight, 0));
+    return 0;
+}
+
+int setVertex(int index, string name, WeightType weight, bool boolPar)
+{
+    vertexInfo[index].Name = name;
+    vertexInfo[index].Weight = weight;
+    vertexInfo[index].BoolPar = boolPar;
+    return 0;
+}
+
+
+int CreateGraph(int vertexCount)
+{
+    graph = Graph (vertexCount);
+    for(int i=0; i<vertexCount; i++)
+        graph[i] = set<Connected, comparer>();
+    vertexInfo = vector<Vertex>(vertexCount);
+    for(int i=0; i<vertexCount; i++)
+        vertexInfo[i] = Vertex(i, "", 0.0, false);
+    return 0;
+}
+
+
+int markEdge(int v1, int v2, StateType stateValue, set<Connected>::iterator &edge1, set<Connected>::iterator &edge2)
+{
+    for(set <Connected>::iterator i=graph[v1].begin(); i != graph[v1].end(); i++)
+    {
+        if((*i).secondVertexIndex == v2)
+        {
+            Connected c = *i;
+            c.state = stateValue;
+            graph[v1].erase(i);
+			edge1 = graph[v1].insert(c).first;
+            break;
+        }
+    }
+    for(set<Connected>::iterator i=graph[v2].begin(); i != graph[v2].end(); i++)
+    {
+        if((*i).secondVertexIndex == v1)
+        {
+            Connected c = *i;
+            c.state = stateValue;
+            graph[v2].erase(i);
+			edge2 = graph[v2].insert(c).first;
+            break;
+        }
+    }
+    return 0;
+}
+
 
 int WideSearch (int vertexIndex)
 {
@@ -74,7 +140,6 @@ int WideSearch (int vertexIndex)
         {
              if (! vertexInfo[(*j).secondVertexIndex].BoolPar)
            {
-//			    ResultVertexes.push_back(graph[CurrentLevel[i]][j].secondVertexIndex);
                 NextLevel.push_back((*j).secondVertexIndex);
                 vertexInfo[(*j).secondVertexIndex].BoolPar=true;
            }
@@ -93,32 +158,6 @@ int WideSearch (int vertexIndex)
    }
    ResultVertexes.clear();
    return 0;
-}
-
-int CreateGraph(int vertexCount)
-{
-    graph = Graph (vertexCount);
-    for(int i=0; i<vertexCount; i++)
-        graph[i] = set<Connected, comparer>();
-    vertexInfo = vector<Vertex>(vertexCount);
-    for(int i=0; i<vertexCount; i++)
-        vertexInfo[i] = Vertex(i, "", 0.0, false);
-    return 0;
-}
-
-int AddEdge(int v1, int v2, WeightType weight)
-{
-    graph[v1].insert(Connected(v2, weight, 0));
-    graph[v2].insert(Connected(v1, weight, 0));
-    return 0;
-}
-
-int setVertex(int index, string name, WeightType weight, bool boolPar)
-{
-    vertexInfo[index].Name = name;
-    vertexInfo[index].Weight = weight;
-    vertexInfo[index].BoolPar = boolPar;
-    return 0;
 }
 
 
@@ -152,32 +191,6 @@ int DepthSearch(int vertexIndex)
     return 0;
 }
 
-int markEdge(int v1, int v2, StateType stateValue, set<Connected>::iterator &edge1, set<Connected>::iterator &edge2)
-{
-    for(set <Connected>::iterator i=graph[v1].begin(); i != graph[v1].end(); i++)
-    {
-        if((*i).secondVertexIndex == v2)
-        {
-            Connected c = *i;
-            c.state = stateValue;
-            graph[v1].erase(i);
-			edge1 = graph[v1].insert(c).first;
-            break;
-        }
-    }
-    for(set<Connected>::iterator i=graph[v2].begin(); i != graph[v2].end(); i++)
-    {
-        if((*i).secondVertexIndex == v1)
-        {
-            Connected c = *i;
-            c.state = stateValue;
-            graph[v2].erase(i);
-			edge2 = graph[v2].insert(c).first;
-            break;
-        }
-    }
-    return 0;
-}
 
 int BasedTreeSearch(int vertexIndex)
 {   stack< pair<int, int> > Stack;
@@ -265,7 +278,6 @@ int ConditionWideSearch (int vertexIndex, int toVertex, vector <int> &result)
 }
 
 
-
 int BaseCircleSearch()
 {
     vector <int> result;
@@ -290,27 +302,170 @@ int BaseCircleSearch()
     return 0;
 }
 
+int dijkstra(int vertexIndex)
+{
+	for (int i = 0; i < vertexInfo.size(); i++)
+	{
+		vertexInfo[i].Weight = -1;
+		vertexInfo[i].BoolPar = false;
+	}
+	vertexInfo[vertexIndex].Weight = 0;
+
+	set <dijkstra_elem> toCheck;
+	toCheck.insert(make_pair(vertexIndex, 0));
+
+	dijkstra_elem current;
+	while (!toCheck.empty())
+	{
+		current = *toCheck.begin();
+		vertexInfo[current.first].BoolPar = true;
+		toCheck.erase(toCheck.begin());
+
+		for (set<Connected>::iterator i = graph[current.first].begin();
+			i != graph[current.first].end(); i++)
+			if (!vertexInfo[(*i).secondVertexIndex].BoolPar)
+			{
+				if (vertexInfo[(*i).secondVertexIndex].Weight == -1)
+				{
+					vertexInfo[(*i).secondVertexIndex].Weight = current.second + 
+						(*i).Weight;
+					toCheck.insert(make_pair((*i).secondVertexIndex,
+						vertexInfo[(*i).secondVertexIndex].Weight));
+				}
+				else
+				{
+					if (vertexInfo[(*i).secondVertexIndex].Weight > current.second +
+						(*i).Weight)
+					{
+						set <dijkstra_elem>::iterator temp = 
+							toCheck.find(make_pair((*i).secondVertexIndex,
+							vertexInfo[(*i).secondVertexIndex].Weight));
+						toCheck.erase(temp);
+						vertexInfo[(*i).secondVertexIndex].Weight = current.second +
+						(*i).Weight;
+						toCheck.insert(make_pair((*i).secondVertexIndex,
+						vertexInfo[(*i).secondVertexIndex].Weight));
+					}
+				}
+			}
+	}
+
+	for (int i = 0; i < vertexInfo.size(); i++)
+		cout << i << " " << vertexInfo[i].Weight << endl;
+
+	return 0;
+}
+
+int FindBridges()
+{   
+	int treeSize = 0;
+	stack< pair<int, int> > Stack;
+	vector <int> marks(graph.size());
+	vector <int> startMarks(graph.size());
+	vector <int> parent(graph.size());
+	
+	for (int i = 0; i < graph.size(); i++)
+		marks[i] = parent[i] = -1;
+
+    Stack = stack< pair<int,int> >();
+    //Stack.push(vertexIndex);
+    vertexInfo[0].BoolPar = true;
+	marks[0] = startMarks[0] = treeSize++;
+    for(set <Connected>::iterator i = graph[0].begin(); i != graph[0].end(); i++)
+    {
+        Stack.push(make_pair(0, (*i).secondVertexIndex));
+    }
+    pair<int, int> p;
+	set <Connected>::iterator edge;
+    while (Stack.size() > 0)
+    {
+        p = Stack.top();
+        Stack.pop();
+        if(!vertexInfo[p.second].BoolPar)
+        {
+            vertexInfo[p.second].BoolPar = true;
+			marks[p.second] = startMarks[p.second] = treeSize++;
+			parent[p.second] = p.first;
+			markEdge(p.first, p.second, 1, edge, edge);
+			int currentFirst = - 1;
+            for(set <Connected>::iterator i = graph[p.second].begin(); i != graph[p.second].end(); i++)
+            {
+				if (marks[(*i).secondVertexIndex] != -1 &&
+					(*i).secondVertexIndex != p.first && (
+					currentFirst == -1 || 
+					marks[currentFirst] > marks[(*i).secondVertexIndex]))
+					currentFirst = (*i).secondVertexIndex;
+                Stack.push(make_pair(p.second, (*i).secondVertexIndex));
+            }
+			if (currentFirst != -1)
+			{
+				int currentIndex = p.second;
+				while (currentIndex != currentFirst)
+				{
+					marks[currentIndex] = marks[currentFirst];
+					currentIndex = parent[currentIndex];
+				}
+			}
+        }
+    }
+
+	for (int i = 1; i < graph.size(); i++)
+	{
+		if (marks[parent[i]] < marks[i])
+			cout << "Мост найден: " << parent[i] << ' ' << i << endl;
+	}
+	cout << endl;
+
+	int children = 0;
+	for (int i = 1; i < graph.size(); i++)
+		children += (parent[i] == 0);
+	if (children > 1)
+		cout << "Точка сочленения: " << 0 << endl;
+
+	for (int i = 1; i < graph.size(); i++)
+	{
+		//if (marks[i] == startMarks[i])
+		{
+			children = 0;
+			bool p = false;
+			for (set<Connected>::iterator j = graph[i].begin(); j != graph[i].end(); j++)
+				if ((*j).secondVertexIndex != parent[i])
+				{
+					p = p || (marks[(*j).secondVertexIndex] >= startMarks[i]);
+					children += (marks[(*j).secondVertexIndex] > marks[i]);
+				}
+			if (children > 1 || p)
+				cout << "Точка сочленения: " << i << endl;
+		}
+	}
+
+	for (int i = 0; i < graph.size(); i++)
+		cout << i << ' ' << marks[i] << ';' << startMarks[i] << endl;
+    return 0;
+}
+
 int ConstGraph()
 {
     int N;
-    N = 7;
+    N = 9;
     CreateGraph(N);
-    AddEdge(0,1,1);
-    AddEdge(0,2,1);
-    AddEdge(1,2,1);
-    AddEdge(1,3,1);
-    AddEdge(1,4,1);
-    AddEdge(2, 5, 1);
-    AddEdge(2,6,1);
-    AddEdge(3,4,1);
-    AddEdge(5,6,1);
-    AddEdge(4,5,1);
+    AddEdge(0,8,1);
+    AddEdge(0,7,10);
+	AddEdge(2,7,10);
+	AddEdge(8,1,10);
+    AddEdge(1,3,2);
+    AddEdge(1,4,5);
+    AddEdge(2,5,2);
+	//AddEdge(7,8,1);
+    AddEdge(2,6,3);
+    AddEdge(3,4,2);
+    AddEdge(5,6,4);
     return 0;
 }
 
 int InputGraph()
 {
-    setlocale(0,"");
+    
     cout<<"Количество вершин=";
     int N;
     cin>>N;
@@ -341,14 +496,18 @@ int InputGraph()
     return 0;
 }
 
+
 int main()
 {
+	setlocale(LC_ALL,"Russian");
     //InputGraph();
     ConstGraph();
-    //WideSearch(0);
-    BasedTreeSearch(5);
-    BaseCircleSearch();
-    //system("PAUSE");
+	//dijkstra(0);
+	FindBridges();
+    //WideSearch(5);
+   // BasedTreeSearch(5);
+    //BaseCircleSearch();
+    system("PAUSE");
     return 0;
 }
 
